686db14f4206e060b80064668a0442873d9d71b3
Germiona <violetta.ashirova@gmail.com>
2019-03-03 18:26:11 +0300

    some changes

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
-import makeFilter from './make-filter.js';
+import {filters} from './make-filter.js';
 import makeTask from './make-task.js';
 
-const filters = document.querySelector(`.main__filter`);
+const mainFilter = document.querySelector(`.main__filter`);
 const tasksBoard = document.querySelector(`.board__tasks`);
 const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`All`, getRandomNum(1, 10)));
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`Overdue`, getRandomNum(1, 10)));
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`Today`, getRandomNum(1, 10)));
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`Favorites`, getRandomNum(1, 10)));
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`Repeating`, getRandomNum(1, 10)));
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`Tags`, getRandomNum(1, 10)));
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`Archive`, getRandomNum(1, 10)));
+filters.forEach((filter) => {
+  let input = document.createElement(`input`);
+  input.classList.add(`filter__input`, `visually-hidden`);
+  input.setAttribute(`id`, `filter__` + filter.title.toLowerCase());
+  input.setAttribute(`type`, `radio`);
+  input.setAttribute(`name`, `filter`);
+  let label = document.createElement(`label`);
+  label.className = `filter__label`;
+  label.setAttribute(`for`, `filter__` + filter.title.toLowerCase());
+  label.innerHTML = `<span>` + filter.title + ` ` + filter.number + `</span>`;
+  mainFilter.appendChild(input);
+  mainFilter.appendChild(label);
+});
 
 const renderTasks = (dist, num) => {
   const tasks = new Array(7)
@@ -27,5 +33,3 @@ filterLabel.forEach((el) => {
     renderTasks(tasksBoard, getRandomNum(5, 15));
   });
 });
-
-
